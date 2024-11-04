5754eb9274532132d49a3fedcb051f79c9741941
suhaib100 <khansuhaib001@gmail.com>
2024-11-04 17:45:24 +0530

    added conversionOperations.js code

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,28 @@
+let score = "hitesh";
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" : 33
+// "33 abc" : NaN
+// true :  1  , fallse : 0
+
+
+// 1 : true ,  0 : false
+// "" : false
+//"hitesh" : truue
+
+let isLoggedIn = "suhaib";
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+
+let scoreNuumber = 33;
+let stringNumber = String(scoreNuumber);
+console.log(stringNumber);
+console.log( typeof stringNumber);
