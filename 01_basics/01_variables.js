5c4e043f5e774a8a5702478929e342d262274949
suhaib100 <khansuhaib001@gmail.com>
2024-11-04 16:26:27 +0530

    added datatypes code also

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 144553
+let accountEmail = "khansuhaib001"
+
+var accountPassword = "12345"
+
+accountCity  = "delhi"
+
+
+
+let accountState;
+
+//accountId  = 1  //not allowed
+
+ accountEmail = "sahil@gmaiil.com"
+ accountPassword = "43254"
+accountCity  = "noida"
+
+/*
+
+prefer not to use var becoz of issue in block scope and functional scope 
+*/
+
+console.log(accountId);
+
+console.table([accountId ,  accountEmail , accountPassword, accountCity , accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js code as newer versiona
+
+// alert(3 + 3) // we are using nodejs ,  not browser
+
+
+
+console.log(3 + 3);
+console.log("samad");
+
+
+
+let name = "samad";
+let number  = 13;
+let isLoggrdIn = false;
+let state;
+
+
+/*
+
+datatypes in js :
+1 string : ""
+2 number : 2 to power 53
+3 boolean : truue/false
+4 null : standalone value
+5 undefined : 
+6 symbol : unique
+7 object  
+*/
+
+console.log(typeof undefined); // uundefined 
+console.log(typeof null); // object 
