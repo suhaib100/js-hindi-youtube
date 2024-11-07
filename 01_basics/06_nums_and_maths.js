ef4076e4b21249e58ee33f14e11740318209b48c
suhaib100 <khansuhaib001@gmail.com>
2024-11-07 13:26:53 +0530

    added 06_nums_and_maths.js and 07_datesinJs.js  code

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,14 @@
+const score = 400;
+const newScore = new Number(100);
+console.log(score);
+console.log(newScore);
+console.log(newScore.toString());
+const n = 123.653;
+console.log(n.toPrecision(3));
+
+const h = 1000000;
+console.log(h.toLocaleString('en-IN'));
+
+///////  math /////////
+console.log(Math.min(2,3,4,325,6));
+console.log(Math.random());
---
+++ 01_basics/07_datesInJs.js
@@ -0,0 +1,12 @@
+const d = new Date();
+// console.log(d.toDateString());
+// console.log(d.toString());
+// console.log(d.toLocaleString());
+//console.log(typeof d);
+let newDatw = new Date(2023 , 0 , 12);
+console.log(newDatw);
+console.log(newDatw.toDateString());
+
+
+const d2 = new Date("2024-11-14");
+console.log(d2.toDateString());
